4241dd6f708e1b1ac2f0e879beead6b8e198791f
west-github <westcoder@outlook.com>
2024-05-10 15:48:01 +0100

    Failed to implement crp in rust

--- rust/src/dp/crp/mod.rs
+++ rust/src/dp/crp/mod.rs
@@ -1,134 +1,46 @@
-// use anyhow::Result;
+use std::fmt::Debug;
 
-// pub trait Handler {
-//     fn execute(&self) -> Result<()>;
+mod old;
 
-//     fn next(self, next: Box<dyn Handler>) -> Box<dyn Handler>;
-// }
+trait IHandler<T> {
+    fn next(&self, request: &T) -> Option<&dyn IHandler<T>>;
 
-// #[derive(Default)]
-// pub struct Router {
-//     handler: Option<Box<dyn Handler>>,
-// }
-
-// impl Handler for Router {
-//     fn execute(&self) -> Result<()> {
-//         todo!()
-//     }
-
-//     fn next(self, next: Box<dyn Handler>) -> Box<dyn Handler> {
-//         todo!()
-//     }
-// }
-
-// pub struct Order {
-//     name: String,
-// }
-
-// impl Order {
-//     pub fn new(name: String) -> Self {
-//         Self { name }
-//     }
-// }
-
-// impl Handler for Order {
-//     fn execute(&self) -> Result<()> {
-//         println!("Executing the order {}", self.name);
-
-//         Ok(())
-//     }
-
-//     fn next(self, next: Box<dyn Handler>) -> Box<dyn Handler> {
-//         todo!()
-//     }
-// }
-
-// #[cfg(test)]
-// mod crp_tests {
-//     use anyhow::Result;
-
-//     use super::{Handler, Order, Router};
-
-//     #[test]
-//     fn crp_test() -> Result<()> {
-//         let router = Router::default()
-//             .next(Box::new(Order::new(String::from("Order 1"))))
-//             .next(Box::new(Order::new(String::from("Order 2"))));
-
-use anyhow::{anyhow, Result};
-
-trait RouterNode {
-    fn process(&self) -> Result<()>;
-}
-
-struct Order {
-    order_name: String,
-}
-
-impl Order {
-    fn new(order_name: String) -> Self {
-        Order { order_name }
+    fn handle(&self, request: &T) {
+        self.next(request).map(|next| next.handle(request));
     }
 }
 
-impl RouterNode for Order {
-    fn process(&self) -> Result<()> {
-        println!("Processing order: {}", self.order_name);
+struct Monkey<'a, T> {
+    name: String,
+    error: bool,
 
-        Ok(())
+    next: Option<&'a dyn IHandler<T>>,
+}
+
+impl<'a, T> Monkey<'a, T> {
+    fn new(name: String, error: bool, next: Option<&'a dyn IHandler<T>>) -> Self {
+        Self { name, error, next }
     }
 }
 
-struct Order2 {
-    order_name: String,
-}
-
-impl Order2 {
-    fn new(order_name: String) -> Self {
-        Order2 { order_name }
-    }
-}
-
-impl RouterNode for Order2 {
-    fn process(&self) -> Result<()> {
-        println!("Processing order: {}", self.order_name);
-
-        Err(anyhow!("Yes we got an error"))
-    }
-}
-
-struct Router {
-    nodes: Vec<Box<dyn RouterNode>>,
-}
-
-impl Router {
-    fn default() -> Self {
-        Router { nodes: Vec::new() }
+impl<'a, T: Debug> IHandler<T> for Monkey<'a, T> {
+    fn next(&self, request: &T) -> Option<&'a dyn IHandler<T>> {
+        self.next
     }
 
-    fn next(mut self, node: Box<dyn RouterNode>) -> Self {
-        self.nodes.push(node);
-        self
-    }
-
-    fn process(&self) -> Result<()> {
-        println!("Router Processing");
-
-        for node in &self.nodes {
-            node.process()?;
+    fn handle(&self, request: &T) {
+        if self.error {
+            println!("{} error early so we cutting the chain", self.name);
+            return;
         }
 
-        Ok(())
+        println!("{} approved, {:?}", self.name, request);
+
+        <Monkey<T> as IHandler<T>>::handle(&self, request)
     }
 }
 
 #[test]
-fn crp_test() -> Result<()> {
-    let router = Router::default()
-        .next(Box::new(Order2::new(String::from("Order 1"))))
-        .next(Box::new(Order::new(String::from("Order 2"))));
-
-    router.process()?;
-
-    Ok(())
+fn crp_test() {
+    let monkey = Monkey::new("Banana".into(), false, Some(&Monkey::new("Nut".into(), false, None)));
 }
